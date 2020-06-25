735f6f4168bf9780e03c2faa06dc713ce2ef2303
bosley <bosley117@gmail.com>
2020-06-25 08:28:31 -0400

    Exploratory-Rework

---
+++ src/del/Managers/SymbolTable.hpp
@@ -0,0 +1,20 @@
+#ifndef DEL_SYMBOL_TABLE
+#define DEL_SYMBOL_TABLE
+
+#include <vector>
+
+namespace DEL
+{
+    class SymbolTable
+    {
+    public:
+        SymbolTable();
+        ~SymbolTable();
+
+    private:
+
+    };
+}
+
+
+#endif
